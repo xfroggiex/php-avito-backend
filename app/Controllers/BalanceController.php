bb53178e8f939eaad8e0683a7506c820b8637775
Froggie <qbninedayz@gmail.com>
2023-03-31 12:17:59 +0300

    Add proper error handling, refactor architecture, and perform full code review

--- app/Controllers/BalanceController.php
+++ app/Controllers/BalanceController.php
@@ -2,39 +2,62 @@
 
 namespace App\Controllers;
 
+use App\Entities\User;
 use App\Models\UsersModel;
+use CodeIgniter\API\ResponseTrait;
+use http\Exception\InvalidArgumentException;
 
 class BalanceController extends BaseController
 {
+    use ResponseTrait;
+
     public function __construct()
     {
         $this->usersModel = new UsersModel();
     }
 
-    public function getBalance(int $user_id = 0)
+    public function validateAmount(float $amount): bool
     {
-        $errors = [];
-        $user = $this->usersModel->find($user_id);
-        if (!isset($user)) {
-            $errors[] = "User does not exist.";
-            return json_encode($errors);
-        } else {
-            $user = json_encode($user);
-            return $user;
+        if ($amount <= 0) {
+            throw new InvalidArgumentException('Invalid amount.');
         }
+        return true;
     }
 
-    public function increaseBalance(int $user_id = 0, float $amount = 0)
+    public function validateUser($user): bool
     {
-        $errors = [];
-        if ($amount <= 0) {
-            $errors[] = "Invalid amount.";
-            return json_encode($errors);
+        if ($user === null) {
+            throw new InvalidArgumentException('User does not exist.');
         }
-        $user = $this->usersModel->find($user_id);
-        if (!isset($user)) {
-            $user = new \App\Entities\User();
-            $user->setUserId($user_id);
+        return true;
+    }
+
+    public function getBalance(int $userId = 0)
+    {
+        $user = $this->usersModel->find($userId);
+
+        try {
+            $this->validateUser($user);
+        } catch (InvalidArgumentException $e) {
+            echo $e->getMessage();
+        }
+
+        return $this->respond($user);
+    }
+
+    public function increaseBalance(int $userId = 0, float $amount = 0)
+    {
+        try {
+            $this->validateAmount($amount);
+        } catch (InvalidArgumentException $e) {
+            echo $e->getMessage();
+        }
+
+        $user = $this->usersModel->find($userId);
+
+        if (!$this->validateUser($user)) {
+            $user = new User();
+            $user->setUserId($userId);
             $user->setBalance($amount);
             $this->usersModel->save($user);
         } else {
@@ -43,67 +66,63 @@ class BalanceController extends BaseController
             $this->usersModel->save($user);
         }
 
-        $user = $this->usersModel->find($user_id);
-        return json_encode($user);
+        return $this->respond($user);
     }
 
-    public function decreaseBalance(int $user_id = 0, float $amount = 0)
+    public function decreaseBalance(int $userId = 0, float $amount = 0)
     {
-        $errors = [];
-        $user = $this->usersModel->find($user_id);
-        if (!isset($user)) {
-            $errors[] = "User does not exist.";
-            return json_encode($errors);
+        $user = $this->usersModel->find($userId);
+
+        try {
+            $this->validateUser($user);
+            $this->validateAmount($amount);
+        } catch (InvalidArgumentException $e) {
+            echo $e->getMessage();
         }
-        if ($amount <= 0) {
-            $errors[] = "Invalid amount.";
-            return json_encode($errors);
-        }
+
         if ($amount > $user->getBalance()) {
-            $errors[] = "Insufficient funds.";
-            return json_encode($errors);
+            return $this->failValidationErrors('Insufficient funds.');
         }
+
         $updatedBalance = $user->getBalance() - $amount;
         $user->setBalance($updatedBalance);
         $this->usersModel->save($user);
 
-        $user = $this->usersModel->find($user_id);
-        return json_encode($user);
+        return $this->respond($user);
     }
 
-    public function transferBalance(int $sender_id = 0, int $recipient_id = 0, float $amount = 0)
+    public function transferBalance(int $senderId = 0, int $recipientId = 0, float $amount = 0)
     {
-        $errors = [];
+        $sender = $this->usersModel->find($senderId);
+        $recipient = $this->usersModel->find($recipientId);
 
-        $sender = $this->usersModel->find($sender_id);
-        $recipient = $this->usersModel->find($recipient_id);
-
-        if (!isset($sender)) {
-            $errors[] = "Sender user does not exist.";
-            return json_encode($errors);
-        } elseif (!isset($recipient)) {
-            $errors[] = "Recipient user does not exist.";
-            return json_encode($errors);
+        if ($sender === null) {
+            return $this->failNotFound('Sender user does not exist.');
         }
 
-        if ($amount <= 0) {
-            $errors[] = "Invalid amount.";
-            return json_encode($errors);
+        if ($recipient === null) {
+            return $this->failNotFound('Recipient user does not exist.');
+        }
+
+        try {
+            $this->validateAmount($amount);
+        } catch (InvalidArgumentException $e) {
+            echo $e->getMessage();
         }
 
         if ($amount > $sender->getBalance()) {
-            $errors[] = "Insufficient funds.";
-            return json_encode($errors);
+            return $this->failValidationErrors('Insufficient funds.');
         }
 
         $senderUpdatedBalance = $sender->getBalance() - $amount;
         $recipientUpdatedBalance = $recipient->getBalance() + $amount;
+
         $sender->setBalance($senderUpdatedBalance);
         $this->usersModel->save($sender);
+
         $recipient->setBalance($recipientUpdatedBalance);
         $this->usersModel->save($recipient);
 
-        $users = $this->usersModel->find([$sender_id, $recipient_id]);
-        return json_encode($users);
+        return $this->respond([$sender, $recipient]);
     }
 }
